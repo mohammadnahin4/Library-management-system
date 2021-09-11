f3c99f4e034a160407c67d1dd0fab13c51535423
mohammadnahin4 <62350046+mohammadnahin4@users.noreply.github.com>
2021-09-11 22:35:01 +0600

    Update footer.php

--- CSE470_MVC STRUCTURE/VIEW/includes/footer.php
+++ CSE470_MVC STRUCTURE/VIEW/includes/footer.php
@@ -2,7 +2,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
                 </div>
 
             </div>
